946474b0ce1555425faa325ce756faeae8f22d3f
Khan Imran <erkhanimrancs@gmail.com>
2023-04-30 21:28:05 +0530

    initial commit

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -1,5 +1,5 @@
 package com.driver;
-
+import java.lang.Iterable;
 public class Order {
 
     private String id;
@@ -9,8 +9,27 @@ public class Order {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+        this.id=id;
+        this.deliveryTime=convrt(deliveryTime);
     }
-
+    private int convrt(String dT){
+        String h="";
+        String m="";
+        boolean met=false;
+        for(char c:dT){
+            if(c==':'){
+                met=true;
+                continue;
+            }
+            if(met){
+                m+=c;
+            }
+            else{
+                h+=c;
+            }
+        }
+        return (Integer.parseInt(h))*60+(Integer.parseInt(m));
+    }
     public String getId() {
         return id;
     }
